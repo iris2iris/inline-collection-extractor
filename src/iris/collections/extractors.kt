5b9bd0908f348fd6010181b264856220da665c4c
iris2iris <33526313+iris2iris@users.noreply.github.com>
2020-09-02 18:45:03 +0400

    First commit

--- src/iris/collections/extractors.kt
+++ src/iris/collections/extractors.kt
@@ -9,143 +9,143 @@ package iris.collections
 inline fun <E, R> Iterable<E>.extract(
 		filter: (item: E) -> Boolean = { true }
 		, breakIfResult: (it: Collection<R>) -> Boolean = { false }
-		, @Suppress("UNCHECKED_CAST") map: (it: E) -> R = { it as R }): Collection<R>
-{
-	val result = mutableListOf<R>()
+		, @Suppress("UNCHECKED_CAST") map: (it: E) -> R = { it as R }
+		, destination: MutableCollection<R> = mutableListOf()
+): Collection<R> {
 	for (item in this) {
 		if (!filter(item))
 			continue
-		result += map(item)
-		if (breakIfResult(result))
-			return result
+		destination += map(item)
+		if (breakIfResult(destination))
+			return destination
 	}
-	return result
+	return destination
 }
 
 inline fun <E, R> Array<E>.extract(
 		filter: (item: E) -> Boolean = { true }
 		, breakIfResult: (it: Collection<R>) -> Boolean = { false }
-		, map: (it: E) -> R = { it as R }): Collection<R>
-{
-	val result = mutableListOf<R>()
+		, map: (it: E) -> R = { it as R }
+		, destination: MutableCollection<R> = mutableListOf()
+): Collection<R> {
 	for (item in this) {
 		if (!filter(item))
 			continue
-		result += map(item)
-		if (breakIfResult(result))
-			return result
+		destination += map(item)
+		if (breakIfResult(destination))
+			return destination
 	}
-	return result
+	return destination
 }
 
 inline fun <R> IntArray.extract(
 		filter: (item: Int) -> Boolean = { true }
 		, breakIfResult: (it: Collection<R>) -> Boolean = { false }
-		, map: (it: Int) -> R = { it as R }): Collection<R>
-{
-	val result = mutableListOf<R>()
+		, map: (it: Int) -> R = { it as R }
+		, destination: MutableCollection<R> = mutableListOf()
+): Collection<R> {
 	for (item in this) {
 		if (!filter(item))
 			continue
-		result += map(item)
-		if (breakIfResult(result))
-			return result
+		destination += map(item)
+		if (breakIfResult(destination))
+			return destination
 	}
-	return result
+	return destination
 }
 
 inline fun <R> LongArray.extract(
 		filter: (item: Long) -> Boolean = { true }
 		, breakIfResult: (it: Collection<R>) -> Boolean = { false }
-		, map: (it: Long) -> R = { it as R }): Collection<R>
-{
-	val result = mutableListOf<R>()
+		, map: (it: Long) -> R = { it as R }
+		, destination: MutableCollection<R> = mutableListOf()
+): Collection<R> {
 	for (item in this) {
 		if (!filter(item))
 			continue
-		result += map(item)
-		if (breakIfResult(result))
-			return result
+		destination += map(item)
+		if (breakIfResult(destination))
+			return destination
 	}
-	return result
+	return destination
 }
 
 inline fun <R> CharArray.extract(
 		filter: (item: Char) -> Boolean = { true }
 		, breakIfResult: (it: Collection<R>) -> Boolean = { false }
-		, map: (it: Char) -> R = { it as R }): Collection<R>
-{
-	val result = mutableListOf<R>()
+		, map: (it: Char) -> R = { it as R }
+		, destination: MutableCollection<R> = mutableListOf()
+): Collection<R> {
 	for (item in this) {
 		if (!filter(item))
 			continue
-		result += map(item)
-		if (breakIfResult(result))
-			return result
+		destination += map(item)
+		if (breakIfResult(destination))
+			return destination
 	}
-	return result
+	return destination
 }
 
 inline fun <R> DoubleArray.extract(
 		filter: (item: Double) -> Boolean = { true }
 		, breakIfResult: (it: Collection<R>) -> Boolean = { false }
-		, map: (it: Double) -> R = { it as R }): Collection<R>
-{
-	val result = mutableListOf<R>()
+		, map: (it: Double) -> R = { it as R }
+		, destination: MutableCollection<R> = mutableListOf()
+): Collection<R> {
 	for (item in this) {
 		if (!filter(item))
 			continue
-		result += map(item)
-		if (breakIfResult(result))
-			return result
+		destination += map(item)
+		if (breakIfResult(destination))
+			return destination
 	}
-	return result
+	return destination
 }
 
 inline fun <R> FloatArray.extract(
 		filter: (item: Float) -> Boolean = { true }
 		, breakIfResult: (it: Collection<R>) -> Boolean = { false }
-		, map: (it: Float) -> R = { it as R }): Collection<R>
-{
-	val result = mutableListOf<R>()
+		, map: (it: Float) -> R = { it as R }
+		, destination: MutableCollection<R> = mutableListOf()
+): Collection<R> {
 	for (item in this) {
 		if (!filter(item))
 			continue
-		result += map(item)
-		if (breakIfResult(result))
-			return result
+		destination += map(item)
+		if (breakIfResult(destination))
+			return destination
 	}
-	return result
+	return destination
 }
 
 inline fun <R> ByteArray.extract(
 		filter: (item: Byte) -> Boolean = { true }
 		, breakIfResult: (it: Collection<R>) -> Boolean = { false }
-		, map: (it: Byte) -> R = { it as R }): Collection<R>
-{
-	val result = mutableListOf<R>()
+		, map: (it: Byte) -> R = { it as R }
+		, destination: MutableCollection<R> = mutableListOf()
+): Collection<R> {
 	for (item in this) {
 		if (!filter(item))
 			continue
-		result += map(item)
-		if (breakIfResult(result))
-			return result
+		destination += map(item)
+		if (breakIfResult(destination))
+			return destination
 	}
-	return result
+	return destination
 }
 
 inline fun <R> ShortArray.extract(
 		filter: (item: Short) -> Boolean = { true }
 		, breakIfResult: (it: Collection<R>) -> Boolean = { false }
-		, map: (it: Short) -> R = { it as R }): Collection<R>
-{
-	val result = mutableListOf<R>()
+		, map: (it: Short) -> R = { it as R }
+		, destination: MutableCollection<R> = mutableListOf()
+): Collection<R> {
 	for (item in this) {
 		if (!filter(item))
 			continue
-		result += map(item)
-		if (breakIfResult(result))
-			return result
+		destination += map(item)
+		if (breakIfResult(destination))
+			return destination
 	}
-	return result
+	return destination
 }
